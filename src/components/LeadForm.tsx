47e002c7e2f335682bc3e212445849ac00cce1c1
Shema Christian <149377256+Chris-Shema-1@users.noreply.github.com>
2025-08-14 14:45:06 +0300

    Update LeadForm.tsx

--- src/components/LeadForm.tsx
+++ src/components/LeadForm.tsx
@@ -38,7 +38,7 @@ export const LeadForm = () => {
         source: "lead-form"
       };
 
-      const webhookUrl = "https://n8n-b19k.onrender.com/webhook/7b3ca633-814c-474b-8828-57d82ce5784d";
+      const webhookUrl = "https://n8n-b19k.onrender.com/webhook-test/7b3ca633-814c-474b-8828-57d82ce5784d";
       
       const response = await fetch(webhookUrl, {
         method: "POST",
